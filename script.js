f1b2ec3605fdc2e4d34694f657a5aae96c0c16fc
eliptik <adamfeyk5@gmail.com>
2023-04-25 22:31:01 +0300

    Set up html & css, add createBoard & render methods

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Gameboard module
 const Gameboard = (() => {
-    const board = [0,0,0,0,0,0,0,0,0];
-    return { board }
+    const board = 
+    [
+        0,0,0,
+        0,0,0,
+        0,0,0
+    ];
+    const container = document.getElementById("game-board")
+    const createBoard = () => {
+        for(let i = 0; i<9; i++){
+        const square = document.createElement("div")
+        square.classList.add("square")
+        container.append(square)
+        }
+    }
+    const render = ()=> {
+        board.forEach((marker, index)=> {
+            container.childNodes[index].textContent = marker
+        })
+    }
+    return { board, createBoard, render }
 })()
 
 // createPlayer factory function
