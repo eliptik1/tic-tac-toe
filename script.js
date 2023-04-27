4eb16c0a37c8dafa06f2a13d348c49a482d51ce2
eliptik <adamfeyk5@gmail.com>
2023-04-27 20:10:58 +0300

    Update the playTurn function, prevent clicking the taken squares

--- script.js
+++ script.js
@@ -16,11 +16,9 @@ const Gameboard = (() => {
         square.classList.add("square")
         container.append(square)
         }
-        console.log("board created")
         render()
     }
     const render = ()=> {
-        console.log("rendered")
         board.forEach((marker, index)=> {
             container.childNodes[index].textContent = marker
         })
@@ -51,20 +49,16 @@ const Game = (() => {
     const playTurn = () => {
         container.addEventListener("click", (e)=> {
             let squareIndex = Array.from(container.childNodes).indexOf(e.target)
-            if(Gameboard.board[squareIndex] === _currentPlayer.marker) {
+            if(Gameboard.board[squareIndex] === "") {
+                Gameboard.board[squareIndex] = _currentPlayer.marker
                 switchPlayer()
-                Gameboard.board[squareIndex] = _currentPlayer.marker
-            } else {
-                Gameboard.board[squareIndex] = _currentPlayer.marker
             }
             Gameboard.render()
-            switchPlayer()
         })
     }
 
     const switchPlayer = () => {
         _currentPlayer = _currentPlayer === _player1 ? _player2 : _player1
-        console.log("player switched")
     }
     return { playTurn }
 })()
