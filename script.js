33404fa2f2a387e13cfe7a45e0a8e7b1354987cc
eliptik <adamfeyk5@gmail.com>
2023-04-28 21:45:09 +0300

    Add win, tie and finish game logic

--- script.js
+++ script.js
@@ -22,15 +22,59 @@ const Game = () => {
     const _player1 = createPlayer("player1", "x")
     const _player2 = createPlayer("player2", "o")
     let _currentPlayer = _player1
+    let isFinished = false
     const playTurn = (squareIndex) => {
         Gameboard.addMark(squareIndex, getCurrentPlayer().marker);
+        checkWin()
         switchPlayer()
     }
     const switchPlayer = () => {
         _currentPlayer = _currentPlayer === _player1 ? _player2 : _player1
     }
     const getCurrentPlayer = () => _currentPlayer
-    return { playTurn, getCurrentPlayer }
+    const checkWin = () => {
+        if(Gameboard.board[0] === _currentPlayer.marker &&
+            Gameboard.board[1] === _currentPlayer.marker &&
+            Gameboard.board[2] === _currentPlayer.marker ||
+            Gameboard.board[3] === _currentPlayer.marker &&
+            Gameboard.board[4] === _currentPlayer.marker &&
+            Gameboard.board[5] === _currentPlayer.marker ||
+            Gameboard.board[6] === _currentPlayer.marker &&
+            Gameboard.board[7] === _currentPlayer.marker &&
+            Gameboard.board[8] === _currentPlayer.marker ||
+
+            Gameboard.board[0] === _currentPlayer.marker &&
+            Gameboard.board[3] === _currentPlayer.marker &&
+            Gameboard.board[6] === _currentPlayer.marker ||
+            Gameboard.board[1] === _currentPlayer.marker &&
+            Gameboard.board[4] === _currentPlayer.marker &&
+            Gameboard.board[7] === _currentPlayer.marker ||
+            Gameboard.board[2] === _currentPlayer.marker &&
+            Gameboard.board[5] === _currentPlayer.marker &&
+            Gameboard.board[8] === _currentPlayer.marker ||
+
+            Gameboard.board[0] === _currentPlayer.marker &&
+            Gameboard.board[4] === _currentPlayer.marker &&
+            Gameboard.board[8] === _currentPlayer.marker ||
+            Gameboard.board[2] === _currentPlayer.marker &&
+            Gameboard.board[4] === _currentPlayer.marker &&
+            Gameboard.board[6] === _currentPlayer.marker
+            ) {
+            console.log(`${_currentPlayer.name} wins`)
+            finishGame()
+            console.log(isFinished)
+        } else if(Gameboard.board.filter(mark => mark === "").length === 0){
+            console.log("Game is a tie!")
+            finishGame()
+        }
+    }
+    const finishGame = () => {
+        console.log("finished")
+        isFinished = true
+    }
+    const checkFinished = () => isFinished
+
+    return { playTurn, getCurrentPlayer, checkWin, checkFinished }
 }
 
 //Screen controller module for DOM
@@ -60,7 +104,7 @@ const ScreenController = (() => {
     //Add marks to the squares
     container.addEventListener("click", (e)=> {
         let squareIndex = Array.from(container.childNodes).indexOf(e.target)
-        if(Gameboard.board[squareIndex] === "") {
+        if(Gameboard.board[squareIndex] === "" && game.checkFinished() === false) {
             game.playTurn(squareIndex)
             render()
         }
